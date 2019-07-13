a11c03c02fb4efd70a72410649a24488a4c0a3fb
areeshatehreem <50449369+areeshatehreem@users.noreply.github.com>
2019-07-13 17:57:05 +0500

    wip

---
+++ script.js
@@ -0,0 +1,372 @@
+let resturantManagement = [
+    "decoration",
+    "sittingArramgment",
+    "rooms",
+    "familyHall",
+    "table",
+    "glass",
+    "plates",
+    "fokes",
+    "spoons",
+    "ketchepBottle",
+    "sauses",
+    "pans",
+    "drinks",
+    "tissue",
+    "straws",
+    "knife",
+    "dishSpoons",
+    "ovens"
+] 
+
+let timings = [{
+    deliveryTimingsInHours: 24,
+    resturantTimingsInHours: 24,
+    phoneNumber: 0332145775,
+    facebookEmail: "www.secondWife.com",
+    isDeliveryFree: true ,
+    isOnlineDelivery: true, 
+}
+    
+]
+
+
+let resturantWorkers = [ {
+    name : "sajid",
+    position: "cheif",
+    phoneNumber: 0321546721,
+    email: "sajidKhan@gmail.com",
+    age: 35,
+    address: "nearAirport",
+    qualification: "intermediate",
+
+},
+
+
+{
+    name : "zafer",
+    position: "cheif",
+    phoneNumber: 032455461,
+    email: "zaferAlam@gmail.com",
+    age: 40,
+    address: "near saudabad",
+    qualification: "none",
+    skill: "cheifCourse",
+    
+
+    
+},
+
+
+{
+    name : "jaffer",
+    position: "cheif",
+    phoneNumber: 0210045541,
+    email: "jafferRiaz123@gmail.com",
+    age: 26,
+    address: "near quaidabad",
+    qualification: "matric",
+    skill: "cheifCourse",
+
+    
+}]
+
+
+let resturantWaiter = [{
+    name : "ali",
+    position: "waiter",
+    phoneNumber: 021006731,
+    email: "aliShazeb45@gmail.com",
+    age: 20,
+    address: "near quaidabad",
+    qualification: "none",
+    
+
+},
+
+
+{
+    name : "junaid",
+    position: "waiter",
+    phoneNumber: 021001241,
+    email: "junaidKhan@gmail.com",
+    age: 31,
+    address: "near lasipara",
+    qualification: "matric",
+    
+
+},
+
+
+{
+    name : "moiz",
+    position: "waiter",
+    phoneNumber: 0133345541,
+    email: "moizSaleem123@gmail.com",
+    age: 26,
+    address: "near abbasiMarket",
+    qualification: "Bcom",
+    isJob: "partTime",
+
+},
+
+
+{
+    name : "ashenAli",
+    position: "waiter",
+    phoneNumber: 0215636731,
+    email: "ashsen.aliKY@gmail.com",
+    age: 24,
+    address: "near modelColony",
+    qualification: "Mcom",
+},
+
+
+
+{
+    name : "jammie",
+    position: "waiter",
+    phoneNumber: 021457231,
+    email: "jammieNasir@gmail.com",
+    age: 18,
+    address: "near safoora",
+    qualification: "matric",
+},
+
+
+{
+    name : "shahmeer",
+    position: "waiter",
+    phoneNumber: 024556731,
+    email: "shahmeerburhan@gmail.com",
+    age: 28,
+    address: "near quaidabad",
+    qualification: "-",
+
+}
+]
+
+
+
+let resturantOwner =  [{
+    name: "muhmmadMirzaSahah",
+    martialStatus: " married",
+    age: 59,
+    qualification : "ACCA " ,
+    phoneNumber: 033215467,
+    email: "MMS.Shahzeb@gmail.com",
+    address: "DHA",
+    religion: "islam",
+    experience: "16 years",
+    position: "owner",
+    isJob: true,
+    company: "asimJofa",
+
+
+
+},
+
+
+{
+    
+    name: "fahadNaeem",
+    martialStatus: " married",
+    age: 63,
+    qualification : "BBA",
+    phoneNumber: 0232321467,
+    email: "fahadNAeem78@gmail.com",
+    address: "clifton",
+    religion: "islam",
+    experience: "11 years",
+    position: "CEO",
+
+
+},
+
+
+{
+    name: "shameerJahan",
+    martialStatus: " married",
+    age: 45,
+    qualification : "BBA",
+    phoneNumber: 0232321467,
+    email: "shameerJahan@gmail.com",
+    address: "gulshan",
+    religion: "islam",
+    position: "manager",
+    experience: "14 years",
+
+},
+
+
+
+{
+    name: "omerFahad",
+    martialStatus: " married",
+    age: 40,
+    qualification : "CA inComplete",
+    phoneNumber: 0232322367,
+    email: "omerFAhad@gmail.com",
+    address: "gulshan e johar",
+    religion: "islam",
+    position: "Accountant",
+    skill :" fastTyping",
+    experience: "four years",
+
+    
+}]
+
+
+
+let items = [{
+    colddrink: [
+        "cocaCola",
+        "pepsi",
+        "string",
+        "sprite",
+        "fanta",
+        " 7up", 
+        "juices",
+        "tins", 
+        "mineralWater"
+    ]
+},
+
+
+{
+    burgers: [
+        "zinger",
+        "zingerCheeseBurger",
+        "simpleBurger",
+        "spiceyZinger",
+        "beefBurger",
+        "chickenCheeseBurger",
+
+
+    ]
+},
+
+
+{
+    rollParatha: [
+        "simpleRollParatha",
+        "maalaiParatha",
+        "zingerRollParatha",
+        "beefRoll",
+        "chickenRoll",
+        "muttonRoll",
+    ]
+},
+
+
+{
+    seaFood: [
+        "prawns",
+        "fish",
+        "fingerFish",
+        "fishWithKabab",
+        "Clam dishes.",
+        "Crab dishes.",
+        "Fish dishes.",
+        "Lobster dishes.",
+        "Octopus dishes.",
+        "Oyster dishes.",
+        "Shrimp dishes.",
+        "Squid dishes.",
+        "chickenChowmen",
+    ]     
+    
+},
+
+
+{
+    starter: [
+        "lasania",
+        "pasta",
+        "chickenLasania",
+        "vegeLAsania",
+        "chickenPasta",
+        "cheesePasta",
+        "vegePasta",
+        "cheeseLasania",
+        "drumStick",
+        "breadRolls",
+        "poataoSticks",
+    
+    ]
+},
+
+
+{
+
+    parathas: [
+        "cheeseParatha",
+        "chickenParatha",
+        "malaiBotiParatha",
+        "vegeParatha",
+        "simpleParatha",
+        "roti",
+        "tandooriRoti",
+
+
+
+    ]
+       
+
+    
+},
+
+
+{
+
+    salan:[
+        "nihari",
+        "acharGosht",
+        "greenKarahi",
+        "whiteKarahi",
+        "keemma",
+        "tikkaBoti",
+        "seekhKabab",
+        "behariBoti",
+        "chapleeKabab",
+        "redKarahi",
+        "briyani",
+        "paulo",
+        "chineseRise",
+
+
+
+
+    ]
+},
+
+
+{
+ fries:[
+     "fries",
+     "masalaFries",
+     "whiteSauseFries",
+     "cheeseFries",
+
+ ]   
+}]
+
+
+
+
+let delivery = [{
+    name : "moshin",
+    phoneNumber: 0332145321,
+    email: "moshinAwan@gmail.com",
+    address: "house  # 13 near shourabGoht",
+    age: 37,
+    isBike: false,
+    
+
+
+
+
+}
+   
+
+]
